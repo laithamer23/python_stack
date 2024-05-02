c9cddf435682c726ad6cfc8836c9ca0593b0fb5f
laith <lith_2o17a@hotmail.com>
2024-05-02 15:12:51 +0300

    update

---
+++ _python/python_fundamentals/function_basic1.text.py
@@ -0,0 +1,35 @@
+1# printing 5 
+5
+
+#2printing 10
+10
+
+3# printing 5 
+5
+
+4# printing 5
+5
+
+5# printing 5 and none
+5 none 
+
+6# printing 3 , 5 and error
+
+7# printing 25
+
+
+8# printing 100 10 
+
+9# printing 7 14 21
+
+10# printing 8 
+
+11# printing 500 500 300 500 
+
+12# printing 500 500 300 500
+
+13# 500 500 300 300 
+
+14# printing 1 3 2 
+
+15# 1 3 5 5 10 real output 1 3 5 10
