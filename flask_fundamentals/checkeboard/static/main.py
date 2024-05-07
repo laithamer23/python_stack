46fef526fb440dd81e5261a2680b24f585f98508
laith <lith_2o17a@hotmail.com>
2024-05-07 13:20:35 +0300

    new assignment

--- flask_fundamentals/checkeboard/static/main.py
+++ flask_fundamentals/checkeboard/static/main.py
@@ -4,6 +4,8 @@ app = Flask(__name__)
 print(__name__)
 @app.route('/')
 def index():
-    return render_template("index.html")
+    
+    return
+render_template("index.html")
 if __name__== "__main__":
     app.run(debug=True)
