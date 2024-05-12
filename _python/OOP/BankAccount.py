eaa77d1eb573db34161f852688bb1663891fe366
laith <lith_2o17a@hotmail.com>
2024-05-12 16:32:47 +0300

    django_counter

--- _python/OOP/BankAccount.py
+++ _python/OOP/BankAccount.py
@@ -14,14 +14,17 @@ class BankAccount:
                self.balance-=amount
           return self
      
-     def yield_insert(self,amount):
+     def yield_insert(self):
+          self.balance= self.balance*self.rate+self.balance
           print(self.balance)
           return self
-
             
 Account1 =BankAccount(0.01,100)
 Account2  =BankAccount(0.02,200)
 
+user=BankAccount(0.08,300)
+user.deposit_amount(100)
+user.yield_insert()
 
 
 Account1.deposit_amount(1000)
